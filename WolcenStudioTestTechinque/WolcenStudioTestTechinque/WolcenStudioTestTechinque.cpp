15e995449d9c7117b7001d557fda1011db5e275e
Gauthier <gauthier.berod@efrei.net>
2019-01-21 15:31:38 +0100

    Premier commit, base du code et du rappot
    Premier commit, mise en place du repo git, utilisation d'un éditteur de code externe (visual studio 2017). Mise en place du rapport d'avancement.

---
+++ WolcenStudioTestTechinque/WolcenStudioTestTechinque/WolcenStudioTestTechinque.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+int main()
+{
+    int thrust; //amount of thrust
+    
+    // game loop
+    while (1) {
+        int x;
+        int y;
+        int nextCheckpointX; // x position of the next check point
+        int nextCheckpointY; // y position of the next check point
+        int nextCheckpointDist; // distance to the next checkpoint
+        int nextCheckpointAngle; // angle between your pod orientation and the direction of the next checkpoint
+        
+        cin >> x >> y >> nextCheckpointX >> nextCheckpointY >> nextCheckpointDist >> nextCheckpointAngle; cin.ignore();
+        int opponentX;
+        int opponentY;
+        cin >> opponentX >> opponentY; cin.ignore();
+
+        // Write an action using cout. DON'T FORGET THE "<< endl"
+        // To debug: cerr << "Debug messages..." << endl;
+
+
+        // You have to output the target position
+        // followed by the power (0 <= thrust <= 100)
+        // i.e.: "x y thrust"
+        
+        //ajuste thrust when the ship is not align with the destination (limit drifting)
+        if(nextCheckpointAngle > 90 || nextCheckpointAngle < -90){
+            thrust = 0;
+        }
+        else{
+            thrust = 100;
+        }
+
+		//Action to do
+        cout << nextCheckpointX << " " << nextCheckpointY << " " << thrust << endl;
+    }
+}
