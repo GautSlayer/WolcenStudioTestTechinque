39ae8517b8edc63fed6e258322cb51d23d486fab
Gauthier <gauthier.berod@efrei.net>
2019-01-27 17:36:49 +0100

    Ajout du système d'anticipation
    Ajout d'un système d'anticipation. Lors de son premier tour, le vaisseau repère et memorise la position de chaque checkpoint. Ensuite, lors des tours suivant, à l'approche d'un checkpoint, il va faire du découplé, son inertie le menant vers le nextCheckpoint, mais s'orientant déjà vers le prochain nextCheckpoint

--- WolcenStudioTestTechinque/WolcenStudioTestTechinque/WolcenStudioTestTechinque.cpp
+++ WolcenStudioTestTechinque/WolcenStudioTestTechinque/WolcenStudioTestTechinque.cpp
@@ -11,15 +11,26 @@ using namespace std;
  * Auto-generated code below aims at helping you parse
  * the standard input according to the problem statement.
  **/
+ //fonction to copy an array
+int* copyArray(int* arrayToCopy, int size)
+{
+	int* arrayCopied = new int[size];
+	for (int i = 0; i < size; i++)
+	{
+		arrayCopied[i] = arrayToCopy[i];
+	}
+	return arrayCopied;
+}
 
  //fonction to add a int[2] to a list<int*>, made to avoid code repetition during memorization of Chechpoints
-//not working, the list is always empty, cause I have to pass the adress of the list, I think
 void addPointToCPList(list<int*>* list, int* CP, int x, int y)
 {
 	CP[0] = x;
-	CP[1] = x;
-	(*list).push_back(CP);
+	CP[1] = y;
+	(*list).push_back(copyArray(CP, 2));
 }
+
+
 
 int main()
 {
@@ -43,9 +54,14 @@ int main()
 	bool firstLapDone = false; //Indicator if we have done the first lap
 	int lastRegisteredCP[2]; //Last CP memorized 
 	bool firstGameLoop = true; //Bool to know if it's the first gameloop
+	int anticipationDistance = 2000; //Distance when we can start to anticipate the i+1 checkpoint (current checkpoint being i)
+	int anticipationSpeed = 200; //Minimum speed to do the anticipation, otherwise, the ship is slowing down to much and wont reach the point with its momentum
+	list<int*>::iterator currentCheckpoint; //The current Next checkpoint
+	int enteringTheZone = true; //If the ship just entrered in the anticipation zone
 
     // game loop
-    while (1) {
+    while (1) 
+	{
         int x;
 		int y;
         int nextCheckpointX; // x position of the next check point
@@ -77,21 +93,12 @@ int main()
 			if (!listOfCPs.empty())
 			{
 				int* firstCP = listOfCPs.front();
-				list<int*>::iterator it;
-				//show all CP registered, for debug purpose
-				for (it = listOfCPs.begin(); it != listOfCPs.end(); ++it)
-				{
-					cerr << "CP : [" << (*it)[0] << ", " << (*it)[1] << "]" << endl;
-				}
 				//If we have only one CP, we wait for the next CP to change, and we add it, can cause problem if there is only 1 CP in the race
 				if (listOfCPs.size() == 1)
 				{
 					if (lastRegisteredCP[0] != nextCheckpointX && lastRegisteredCP[1] != nextCheckpointY)
 					{
-						lastRegisteredCP[0] = nextCheckpointX;
-						lastRegisteredCP[1] = nextCheckpointY;
-						listOfCPs.push_back(lastRegisteredCP);
-						//addPointToCPList(listOfCPs, lastRegisteredCP, nextCheckpointX, nextCheckpointY);
+						addPointToCPList(&listOfCPs, lastRegisteredCP, nextCheckpointX, nextCheckpointY);
 					}
 				}
 				//if we meet againt the first CP registered, we have done a lap
@@ -99,25 +106,17 @@ int main()
 				{
 					firstLapDone = true;
 					cerr << "Lap Done" << endl;
+					currentCheckpoint = listOfCPs.begin();
 				}
 				//if the current nextCheckPoint is not the last CP registered (not encountered yet), we had it
 				else if(lastRegisteredCP[0] != nextCheckpointX && lastRegisteredCP[1] != nextCheckpointY)
 				{
-					lastRegisteredCP[0] = nextCheckpointX;
-					lastRegisteredCP[1] = nextCheckpointY;
-					listOfCPs.push_back(lastRegisteredCP);
-					
-					//addPointToCPList(listOfCPs, lastRegisteredCP, nextCheckpointX, nextCheckpointY);
+					addPointToCPList(&listOfCPs, lastRegisteredCP, nextCheckpointX, nextCheckpointY);
 				}
 			}
 			else
 			{
-				
-				lastRegisteredCP[0] = nextCheckpointX;
-				lastRegisteredCP[1] = nextCheckpointY;
-				listOfCPs.push_back(lastRegisteredCP);
-				
-				//addPointToCPList(listOfCPs, lastRegisteredCP, nextCheckpointX, nextCheckpointY);
+				addPointToCPList(&listOfCPs, lastRegisteredCP, nextCheckpointX, nextCheckpointY);
 			}
 		}
 		
@@ -129,27 +128,32 @@ int main()
 		movementVector[0] = x - previousX;
 		movementVector[1] = y - previousY;
 
-		//Calcul of "axisVector", the vector from the current position of the ship toward the nextCheckpoint
-		axisVector[0] = nextCheckpointX - x;
-		axisVector[1] = nextCheckpointY - y;
+		//If the movementVector is null, then the ship don't move, so no correction needed
+		//It avoid division by 0
+		if (movementVector[0] != 0 && movementVector[1] != 0)
+		{
+			//Calcul of "axisVector", the vector from the current position of the ship toward the nextCheckpoint
+			axisVector[0] = nextCheckpointX - x;
+			axisVector[1] = nextCheckpointY - y;
 
-		//Calcul of the orthogonal projection of the movement Vector on the axe ship->nextCheckpoint
-		int scalarProduct = (movementVector[0] * axisVector[0]) + (movementVector[1] * axisVector[1]);
-		projOrthoMoveVector[0] = ((float)scalarProduct / (float)pow(nextCheckpointDist, 2)) * axisVector[0];
-		projOrthoMoveVector[1] = ((float)scalarProduct / (float)pow(nextCheckpointDist, 2)) * axisVector[1];
+			//Calcul of the orthogonal projection of the movement Vector on the axe ship->nextCheckpoint
+			int scalarProduct = (movementVector[0] * axisVector[0]) + (movementVector[1] * axisVector[1]);
+			projOrthoMoveVector[0] = ((float)scalarProduct / (float)pow(nextCheckpointDist, 2)) * axisVector[0];
+			projOrthoMoveVector[1] = ((float)scalarProduct / (float)pow(nextCheckpointDist, 2)) * axisVector[1];
 
-		//calcul of the estimated deviation from a straight line, movementVector minus it's orthogonal projection
-		deviationVector[0] = movementVector[0] - projOrthoMoveVector[0];
-		deviationVector[1] = movementVector[1] - projOrthoMoveVector[1];
+			//calcul of the estimated deviation from a straight line, movementVector minus it's orthogonal projection
+			deviationVector[0] = movementVector[0] - projOrthoMoveVector[0];
+			deviationVector[1] = movementVector[1] - projOrthoMoveVector[1];
 
-		//Calcul of the correction vector for the current trajectory
-		float normeOfProjOthro = sqrt(pow(projOrthoMoveVector[0], 2) + pow(projOrthoMoveVector[1], 2));
+			//Calcul of the correction vector for the current trajectory
+			float normeOfProjOthro = sqrt(pow(projOrthoMoveVector[0], 2) + pow(projOrthoMoveVector[1], 2));
 
-		//Use of Thales theorem to find the vector Orthogonal to AxisVector, and colinear to movementVector
-		//Then we use the opposit of this vector as the trajectory correction
-		trajCorrectionVector[0] = -(int)((((float)nextCheckpointDist / (float)normeOfProjOthro) * deviationVector[0]) * (correctionFactor / 100));
-		trajCorrectionVector[1] = -(int)((((float)nextCheckpointDist / (float)normeOfProjOthro) * deviationVector[1]) * (correctionFactor / 100));
-
+			//Use of Thales theorem to find the vector Orthogonal to AxisVector, and colinear to movementVector
+			//Then we use the opposit of this vector as the trajectory correction
+			trajCorrectionVector[0] = -(int)((((float)nextCheckpointDist / (float)normeOfProjOthro) * deviationVector[0]) * (correctionFactor / 100));
+			trajCorrectionVector[1] = -(int)((((float)nextCheckpointDist / (float)normeOfProjOthro) * deviationVector[1]) * (correctionFactor / 100));
+		}
+		
         // Write an action using cout. DON'T FORGET THE "<< endl"
         // To debug: cerr << "Debug messages..." << endl;
 
@@ -163,7 +167,8 @@ int main()
 		{
             thrust = 0;
         }
-		else {
+		else 
+		{
 			thrust = 100;
 		}
 
@@ -174,6 +179,33 @@ int main()
 			thrust = (nextCheckpointDist * 100) / startBakeDistance;
 		}
 		
+		//Anticipation of the i+1 checkpoint (current target checkpoint i)
+		//make the ship turns toward checkpoint i+1, while its momentum make him pass the checkpoint i
+		if (speed >= anticipationSpeed && nextCheckpointDist <= anticipationDistance && firstLapDone)
+		{
+			thrust = 0;
+			//check when we enter for the first time in the anticipation zone, to do currentCheckpoint++ only one time
+			if (enteringTheZone)
+			{
+				currentCheckpoint++;
+				//if we are at the end of the list, we go back to the start
+			//a circular chained list was supposed to be used here, but there is none in the std, I am not sure I can used other .cpp files
+			//Or I am not sure it is worth the time to implemant my own, with this "trick" I am the same effect, saving dev time
+				if (currentCheckpoint == listOfCPs.end())
+				{
+					currentCheckpoint = listOfCPs.begin();
+				}
+				enteringTheZone = false;
+			}
+			
+			nextCheckpointX = (*currentCheckpoint)[0];
+			nextCheckpointY = (*currentCheckpoint)[1];
+		}
+		//reset the entering zone, so, when we enter the next anticipation zone, currentCheckPoint will increase
+		else if (!enteringTheZone && nextCheckpointDist > anticipationDistance && firstLapDone)
+		{
+			enteringTheZone = true;
+		}
 
 		//Use the boost in a big staight line (try to avoid drifting)
 		if (boostAvailable && nextCheckpointAngle == 0 && nextCheckpointDist >= minDistanceForBoost)
@@ -183,12 +215,15 @@ int main()
 		}
         
 		//Debug for in game stats
+		
 		cerr << "speed : " << speed << endl;
+		/*
 		cerr << "Boost available : " << boostAvailable << endl;
 		cerr << "Distance : " << nextCheckpointDist << endl;
 		cerr << "Angle : " << nextCheckpointAngle << endl;
+		*/
 		cerr << "Thrust : " << thrust << endl;
-
+		
 		/*
 		cerr << "Current Position : " << x << " ," << y << endl;
 		cerr << "Moving Vector : [" << movementVector[0] << ", " << movementVector[1] << "]" << endl;
@@ -198,6 +233,18 @@ int main()
 		cerr << "Next Checkpoint Position : " << nextCheckpointX << " ," << nextCheckpointY << endl;
 		cerr << "Traj Correction Vector : [" << trajCorrectionVector[0] << ", " << trajCorrectionVector[1] << "]" << endl;
 		*/
+
+		//show all CP registered, for debug purpose
+		//cerr << "Last registered CP : [" << lastRegisteredCP[0] << ", " << lastRegisteredCP[1] << "]" << endl;
+		list<int*>::iterator it;
+		for (it = listOfCPs.begin(); it != listOfCPs.end(); ++it)
+		{
+			cerr << "CP : [" << (*it)[0] << ", " << (*it)[1] << "]" << endl;
+		}
+		if (firstLapDone)
+		{
+			cerr << "Current Checkpoint : [" << (*currentCheckpoint)[0] << ", " << (*currentCheckpoint)[1] << "]" << endl;
+		}
 
 		//Action to do
 		//Added the vector correction to the coordinates we are aiming
